d10c073ff6d75e6b2027e390e44509f69f89d1b4
James Roncy <roncy.pro13@gmail.com>
2025-08-24 00:15:06 +0800

    added html tags for format content

--- src/api/documents/document.utils.ts
+++ src/api/documents/document.utils.ts
@@ -3,6 +3,8 @@ import { ISectionContent } from "./documents.types";
 export function formatContent(doc: ISectionContent) {
   if (!doc) return;
 
-  const sub = doc.subClause?.length ? `\n- ${doc.subClause.join("\n- ")}` : "";
-  doc.formattedContent = `${doc.section}\n${doc.clause}${sub}`;
+  const sub = doc.subClause?.length
+    ? `\n\n\n\t\t\t<h6>- ${doc.subClause.join("\n- ")}<h6>`
+    : "";
+  doc.formattedContent = `<h3>${doc.section}</h3>\n\n\t\t<h4>${doc.clause}${sub}</h4>`;
 }
